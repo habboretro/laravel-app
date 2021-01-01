b02a2696e93e575c89c251171452a8a8814c3f3e
kanirobinson <me@kanirobinson.co.uk>
2021-01-01 21:22:47 +0000

    Very simple news article page

--- resources/js/pages/news/show.vue
+++ resources/js/pages/news/show.vue
@@ -1,13 +1,31 @@
 <template>
-  <div id="client" class="w-screen h-screen">{{ article }}</div>
+  <section>
+    <jet-card class="max-w-3xl px-4 py-4 pb-12 mx-auto">
+      <jet-card class="p-4 bg-cover bg-center rounded-lg mb-4" :style="`background-image: url('${article.image}');`">
+        <p class="font-bold text-3xl">{{ article.title }}</p>
+        <p>Posted by {{ article.user ? article.user.username : 'The Laravel Team' }} on the {{ article.created_at | formatDate}}</p>
+      </jet-card>
+      <div v-html="article.full_story" />
+    </jet-card>
+  </section>
 </template>
 
 <script>
+import moment from "moment";
 import Layout from "@/layouts/master";
+import JetCard from "@/components/Card";
 export default {
   layout: Layout,
+  components: {
+    JetCard,
+  },
   props: {
     article: Object
+  },
+  filters: {
+    formatDate(date) {
+      return moment(date).format('MMMM d, YYYY');
+    }
   }
 };
 </script>
