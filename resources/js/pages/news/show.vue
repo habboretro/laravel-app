1298200d3be9d5c75f33e70fd92660afcd432e3f
kanirobinson <me@kanirobinson.co.uk>
2021-01-01 22:40:24 +0000

    Cleaned up news page and made it look better

--- resources/js/pages/news/show.vue
+++ resources/js/pages/news/show.vue
@@ -1,12 +1,33 @@
 <template>
-  <section>
-    <jet-card class="max-w-3xl px-4 py-4 pb-12 mx-auto">
-      <jet-card class="p-4 bg-cover bg-center rounded-lg mb-4" :style="`background-image: url('${article.image}');`">
-        <p class="font-bold text-3xl">{{ article.title }}</p>
-        <p>Posted by {{ article.user ? article.user.username : 'The Laravel Team' }} on the {{ article.created_at | formatDate}}</p>
+  <section class="flex flex-wrap md:-mx-4">
+    <div class="w-full md:px-4 md:flex-1 mb-4">
+      <jet-card class="px-4 py-4 pb-12">
+        <jet-card class="p-4 text-white text-center bg-cover bg-center rounded-lg mb-4" :style="`background-image: url('${article.image}');`">
+          <div class="relative w-full py-4">
+            <div class="absolute inset-0 bg-black opacity-50 rounded-lg" />
+            <div class="relative z-10">
+              <p class="font-bold text-3xl">{{ article.title }}</p>
+              <p>{{ article.short_story }}</p>
+            </div>
+          </div>
+        </jet-card>
+        <div v-html="article.full_story" />
       </jet-card>
-      <div v-html="article.full_story" />
-    </jet-card>
+    </div>
+    <div class="w-full md:px-4 md:w-1/3">
+      <jet-card class="p-4">
+        <jet-title-card class="mb-4">Author</jet-title-card>
+        <div class="flex items-center">
+          <div class="flex items-center justify-center w-16 h-16 bg-green-400 rounded-full border-4 border-green-500 mr-4">
+            <img :src="`/habbo-imaging/avatarimage?figure=${article.user ? article.user.look : 'wa-3264-1329-1408.lg-3257-110.sh-3016-110.hr-831-61.fa-1204-0.ch-3059-110.he-3258-93-96.hd-180-1'}&headonly=true`" :alt="article.user ? article.user.username : `${$page.shortname} Team`" class="drop-shadow" />
+          </div>
+          <div class="flex-1">
+            <p class="font-bold mb-1">Posted by {{ article.user ? article.user.username : `${$page.shortname} Team` }}</p>
+            <p class="text-sm">{{ article.created_at | formatDate }}</p>
+          </div>
+        </div>
+      </jet-card>
+    </div>
   </section>
 </template>
 
@@ -14,10 +35,12 @@
 import moment from "moment";
 import Layout from "@/layouts/master";
 import JetCard from "@/components/Card";
+import JetTitleCard from "@/components/TitleCard";
 export default {
   layout: Layout,
   components: {
     JetCard,
+    JetTitleCard,
   },
   props: {
     article: Object
