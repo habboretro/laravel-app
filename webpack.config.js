ac7edb662f5527b229bbc1eaf68e92d02055976a
kanirobinson <me@kanirobinson.co.uk>
2021-01-02 11:03:14 +0000

    Fixed webpack issue with chunking files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
 module.exports = {
+    output: { 
+        chunkFilename: 'js/chunks/[name].js?id=[chunkhash]',
+        publicPath: '/',
+    },
     resolve: {
         alias: {
             '@': path.resolve('resources/js'),
